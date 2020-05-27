824807421f49c93fe4d914ad91c32f50df403533
Federico Briata <federico@briata.org>
2020-05-27 03:10:53 +0200

    risolto set_scale e sleep mode

--- Bilancia-per-alveari.ino
+++ Bilancia-per-alveari.ino
@@ -2,6 +2,8 @@
 #include "sms.h"
 #include <SoftwareSerial.h>
 
+//#define DEBUG_MODE
+
 GSM GSM;
 SMSGSM sms;
 
@@ -19,10 +21,10 @@ const float LOADCELL_DIVIDER = -21.45;  // calibrazione della cella di carico
 
 float units;
 float units_sms;
-char peso[5];                         //char dove inserire il valore di peso da inviare via SMS
+char peso[5];                           //char dove inserire il valore di peso da inviare via SMS
 
-int Reset = 4;                        //dichiaro il Pin per dare l'impulso di reset quando richiesto
-void(* Riavvia)(void) = 0;          //Comando per Software Reset solo con Auduino Uno
+int Reset = 4;                          //dichiaro il Pin per dare l'impulso di reset quando richiesto
+void(* Riavvia)(void) = 0;              //Comando per Software Reset solo con Auduino Uno
 
 void setup() {
     digitalWrite(Reset, HIGH);          //Dichiaro il Pin 4 in che condizione deve stare quando non
@@ -31,9 +33,45 @@ void setup() {
 
     Serial.begin(19200);
 
-    scale.set_scale();
-    scale.tare();
+    // init
+    scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
 
+#ifdef DEBUG_MODE
+    Serial.println("Before setting up the scale:");
+    Serial.print("read: \t\t");
+    Serial.println(scale.read());                 // print a raw reading from the ADC
+
+    Serial.print("read average: \t\t");
+    Serial.println(scale.read_average(20));       // print the average of 20 readings from the ADC
+
+    Serial.print("get value: \t\t");
+    Serial.println(scale.get_value(5));           // print the average of 5 readings from the ADC minus the tare weight (not set yet)
+
+    Serial.print("get units: \t\t");
+    Serial.println(scale.get_units(5), 1);        // print the average of 5 readings from the ADC minus tare weight (not set) divided
+                                                  // by the SCALE parameter (not set yet)
+#endif
+
+    scale.set_scale(LOADCELL_DIVIDER);
+    //loadcell.set_offset(LOADCELL_OFFSET);
+    scale.tare();                                 // reset the scale to 0
+
+#ifdef DEBUG_MODE
+    Serial.println("After setting up the scale:");
+
+    Serial.print("read: \t\t");
+    Serial.println(scale.read());                 // print a raw reading from the ADC
+
+    Serial.print("read average: \t\t");
+    Serial.println(scale.read_average(20));       // print the average of 20 readings from the ADC
+
+    Serial.print("get value: \t\t");
+    Serial.println(scale.get_value(5));           // print the average of 5 readings from the ADC minus the tare weight, set with tare()
+
+    Serial.print("get units: \t\t");
+    Serial.println(scale.get_units(5), 1);        // print the average of 5 readings from the ADC minus tare weight, divided
+                                                  // by the SCALE parameter set with set_scale
+#endif
     Serial.println("ACCENSIONE MODULO GSM...");
 
     digitalWrite(9,HIGH);
@@ -50,20 +88,20 @@ void setup() {
     }
     else Serial.println("STATO Modulo GSM = NON CONNESS0");
 
-    delay(1000);   // Pausa di stabilizzazione dei sensori SHT
+    delay(1000);                                     // Pausa di stabilizzazione dei sensori SHT
 }
 
 void loop() {
 
     char position;
 
-    // init
-    scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-    scale.set_scale(LOADCELL_DIVIDER);
-    //loadcell.set_offset(LOADCELL_OFFSET);
-
-    // Faccio una media di 10 letture e le associo alla voce "units"
-    units = scale.get_units(10);
+    Serial.println("Inizio letture:");
+    Serial.print("prima lettura:\t");
+    Serial.print(scale.get_units(), 1);
+    Serial.print("\t| media:\t");
+    Serial.println(scale.get_units(10), 1);          // Faccio una media di 10 letture meno il peso della tara
+ 
+    units = scale.get_units(10);                     // Faccio una media di 10 letture e le associo alla voce "units"
 
     Serial.print("Peso:");
     Serial.print(units, 0);
@@ -73,7 +111,7 @@ void loop() {
 
     // Legge se ci sono messaggi disponibili sulla SIM Card
     // e li visualizza sul Serial Monitor.
-    position = sms.IsSMSPresent(SMS_UNREAD); // Con questo comando esegue solo gli Sms non letti
+    position = sms.IsSMSPresent(SMS_UNREAD);         // Con questo comando esegue solo gli Sms non letti
     if (position) {
         // Leggo il messaggio SMS e stabilisco chi sia il mittente
         sms.GetSMS(position, Mittente, smsbuffer, 10);
@@ -82,19 +120,19 @@ void loop() {
         Serial.println("\r\n SMS testo:");
         Serial.print(smsbuffer);
 
-        if (strcmp(smsbuffer,"Reset")==0)  {    //Invio sms "Reset" per resettare il programma
+        if (strcmp(smsbuffer,"Reset")==0)  {         //Invio sms "Reset" per resettare il programma
             Serial.println("...Riavvio...");
-            sms.DeleteSMS(position);              //Cancello dalla SIM il comando appenna letto
+            sms.DeleteSMS(position);                 //Cancello dalla SIM il comando appenna letto
             delay(500);
-            Riavvia();                          //Comando si software reset con Arduino Uno
-            //digitalWrite(Reset, LOW);             //Eseguo il comando di Reset mettendo a massa il Pin 4
+            Riavvia();                               //Comando si software reset con Arduino Uno
+            //digitalWrite(Reset, LOW);              //Eseguo il comando di Reset mettendo a massa il Pin 4
         }
-        if (strcmp(smsbuffer,"Misure")==0)  {  //Invio sms "Misure" per ricevere sms con valori
+        if (strcmp(smsbuffer,"Misure")==0)  {        //Invio sms "Misure" per ricevere sms con valori
             units_sms = scale.get_units(5);
             dtostrf(units_sms,0,0,peso);
             snprintf(Misure,20,"Peso=%sg",peso);
             sms.SendSMS(Mittente,Misure);
-            sms.DeleteSMS(position);              //Cancello dalla SIM il comando appenna letto
+            sms.DeleteSMS(position);                 //Cancello dalla SIM il comando appenna letto
             delay(500);
             //lcd.clear();
             //lcd.init();
@@ -103,14 +141,14 @@ void loop() {
         else {
             Serial.println(" => non riconosciuto!");
             sms.SendSMS(Mittente,"Comando Errato");  //risposta in caso di messaggio errato
-            sms.DeleteSMS(position);              //Cancello dalla SIM il comando appenna letto
+            sms.DeleteSMS(position);                 //Cancello dalla SIM il comando appenna letto
             delay(500);
             //lcd.clear();
             //lcd.init();
         }
-        sms.DeleteSMS(position);             //Cancello dalla SIM i messaggi appena letti ed eseguiti
+        sms.DeleteSMS(position);                     //Cancello dalla SIM i messaggi appena letti ed eseguiti
     }
-    // scale.power_down();              // put the ADC in sleep mode
-    // delay(5000);
-    // scale.power_up();
+    scale.power_down();                              // put the ADC in sleep mode
+    delay(5000);
+    scale.power_up();
 }
